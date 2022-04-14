f35dd561ab0290bf036e83cea7e0a8960f820197
Matteo Bianchi <m.bianchi@opengate.biz>
2022-04-14 11:47:35 +0200

    Disabled allauth

--- lazyBids/lazyBids/settings.py
+++ lazyBids/lazyBids/settings.py
@@ -49,9 +49,9 @@ INSTALLED_APPS = [
 
     #External libraries
     #allauth: https://django-allauth.readthedocs.io/en/latest/overview.html
-    'allauth', 
-    'allauth.account',
-    'allauth.socialaccount',
+    # 'allauth', 
+    # 'allauth.account',
+    # 'allauth.socialaccount',
 ]
 
 MIDDLEWARE = [
@@ -147,7 +147,7 @@ AUTHENTICATION_BACKENDS = [
     'django.contrib.auth.backends.ModelBackend',
 
     # `allauth` specific authentication methods, such as login by e-mail
-    'allauth.account.auth_backends.AuthenticationBackend',
+    #'allauth.account.auth_backends.AuthenticationBackend',
 ]
 
 REST_FRAMEWORK = {
