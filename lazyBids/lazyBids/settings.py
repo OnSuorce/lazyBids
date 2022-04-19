e6048c5d6bada92db8b31b485f334284b0d5e9fd
Matteo Bianchi <m.bianchi@opengate.biz>
2022-04-19 16:56:34 +0200

    Configured cors headers, minor changes

--- lazyBids/lazyBids/settings.py
+++ lazyBids/lazyBids/settings.py
@@ -49,7 +49,8 @@ INSTALLED_APPS = [
     'auctions',
 
     # External libraries
-    'djoser'
+    'djoser',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
@@ -60,7 +61,13 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
 ]
+CORS_ORIGIN_ALLOW_ALL = True
+
+CORS_ORIGIN_WHITELIST = (
+  'http://localhost:8000',
+)
 
 ROOT_URLCONF = 'lazyBids.urls'
 
