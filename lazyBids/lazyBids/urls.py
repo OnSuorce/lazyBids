cb5c0b347de26005451feb88212cf692f208ccb0
Matteo Bianchi <m.bianchi@opengate.biz>
2022-04-29 10:59:29 +0200

    Minor change in the swagger

--- lazyBids/lazyBids/urls.py
+++ lazyBids/lazyBids/urls.py
@@ -21,9 +21,9 @@ from drf_yasg import openapi
 
 schema_view = get_schema_view(
    openapi.Info(
-      title="Snippets API",
+      title="lazyBids API",
       default_version='v1',
-      description="Test description",
+      description="DRF api for an auctions service",
       terms_of_service="https://www.google.com/policies/terms/",
       contact=openapi.Contact(email="contact@snippets.local"),
       license=openapi.License(name="BSD License"),
